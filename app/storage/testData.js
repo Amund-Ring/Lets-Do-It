29bbfb373d34a57e81526026dc20385a7b081c37
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-11-14 11:00:09 +0100

    Update testData.js

--- app/storage/testData.js
+++ app/storage/testData.js
@@ -12,42 +12,17 @@ const data = [
     completed: false
   },
   {
+    id: 16248274745233,
+    emoji: '🎸',
+    description: 'Play guitar ',
+    completed: false
+  },
+  {
     id: 1624827474503,
     emoji: '🤓',
     description: 'Finish app ',
     completed: true
-  },
-  {
-    id: 16248273935706,
-    emoji: '🍦',
-    description: 'Buy icecream ',
-    completed: false
-  },
-  {
-    id: 16248274038954,
-    emoji: '🏃',
-    description: 'Go for a jog',
-    completed: false
-  },
-  {
-    id: 16248274745033,
-    emoji: '🤓',
-    description: 'Finish app ',
-    completed: true
-  },
-  {
-    id: 1624827403338954,
-    emoji: '🏃',
-    description: 'Go for a jog',
-    completed: false
-  },
-  {
-    id: 1624827474335033,
-    emoji: '🤓',
-    description: 'Finish app ',
-    completed: true
-  },
-  
+  }
 ];
 
 export default data;
